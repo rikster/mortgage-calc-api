31f34f836217f8548a61c22875d2f38e66724efc
Richard Hounslow <rhounslow@gmail.com>
2020-10-17 17:59:16 +1100

    Get basic express server running

--- server.js
+++ server.js
@@ -2,4 +2,9 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 5000;
 
+//Add Basic Calc Route for mortgage
+// app.get('/', ((req, res) => res.send("Hello World")));
+app.get('/', ((req, res) => res.json({msg: "Hello World"})));
+
+
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
